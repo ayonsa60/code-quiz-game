d12bd398eef3d69e28f4d60a638c050685681767
ayonsa60 <ayonsa60@gmail.com>
2021-10-05 22:54:18 -0500

    code-quiz

---
+++ script.js
@@ -0,0 +1,46 @@
+var question = []; {
+    question[1] = "Which of the following is a correct variable value?";
+    question[2] = "What is an array?";
+    question[3] = "Which of the following is the correct wat to name a variable?";
+    question[4] = "What is the right way to write an array?";
+    question[5] = "Which operator is used to assign a value to a variable?";
+    }
+
+
+
+
+// Which of the following is a correct variable value?
+// Boolean
+// "String"
+// Number
+// All of the above
+
+// What is an array?
+// A group of values that are stored in a particular order
+// data value that can change depending on conditions or information passed to the program
+// A function that displays a defined variable
+// A standalone entity, with properties and type
+
+// Which of the following is the correct wat to name a variable?
+// Hyphen
+// CamelCase
+// Parentheses
+// Commas
+
+// What is the right way to write an array?
+// var = {}
+// var = ""
+// var = ()
+// var = []
+
+// Which operator is used to assign a value to a variable?
+// =
+// :
+// ()
+// *
+
+// <!-- <script> -->
+document.getElementById("bttn").addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = -->
+  });
+  </script>
